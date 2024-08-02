4857afcef8d13ba0e7932caf40b03423bb4e4e39
chrisgteague <chrisgteague@gmail.com>
2024-08-02 13:05:19 +0200

    stuff is working
    Co-Authored-By: TariqBarnabas <104631780+TariqBarnabas@users.noreply.github.com>
    Co-Authored-By: James Pollard <113669215+st10083397@users.noreply.github.com>

--- Semester 6/Accelerometer/sketch_aug2a/sketch_aug2a.ino
+++ Semester 6/Accelerometer/sketch_aug2a/sketch_aug2a.ino
@@ -5,21 +5,20 @@
 #include <DFRobotDFPlayerMini.h>
 #include <SoftwareSerial.h>
 
-
 // Define I2C pins for ESP32
 #define I2C_SDA 21
 #define I2C_SCL 22
-//miniplayer
-#define RX 10 // RX pin of DFPlayer Mini connected to pin 10 of Lilypad
-#define TX 11 // TX pin of DFPlayer Mini connected to pin 11 of Lilypad
-#define TOUCH_PIN 2
+
+// DFPlayer Mini
+#define RX_PIN 12
+#define TX_PIN 13
 
 // Accelerometer
 Adafruit_ADXL345_Unified accel = Adafruit_ADXL345_Unified(12345);
-//miniplayer
-SoftwareSerial mySoftwareSerial(RX, TX); // Create a software serial port
-DFRobotDFPlayerMini myDFPlayer; // Create a DFPlayerMini object
 
+// DFPlayer Mini
+SoftwareSerial mySoftwareSerial(RX_PIN, TX_PIN); // Create a software serial port
+DFRobotDFPlayerMini myDFPlayer; // Create a DFPlayerMini object
 
 // Bluetooth
 BluetoothSerial SerialBT;
@@ -30,6 +29,7 @@ const int fallThreshold = -15;
 
 void setup() {
   Serial.begin(115200);
+  Serial.begin(9600);
   SerialBT.begin("ESP32_BT"); // Bluetooth device name
   Serial.println("The device started, now you can pair it with Bluetooth!");
 
@@ -39,9 +39,17 @@ void setup() {
   // Initialize accelerometer
   if (!accel.begin()) {
     Serial.println("Could not find a valid ADXL345 sensor, check wiring!");
-    
   }
 
+  // Initialize DFPlayer Mini
+  mySoftwareSerial.begin(9600);
+  if (!myDFPlayer.begin(mySoftwareSerial)) {  // Use softwareSerial to communicate with DFPlayer Mini
+    Serial.println("Unable to begin:");
+    Serial.println("1. Please recheck the connection!");
+    Serial.println("2. Please insert the SD card!");
+    while (true);
+  }
+  myDFPlayer.volume(30);  // Set volume to 30 (0~30).
   
   SerialBT.println("Testing Connection");
 }
@@ -65,16 +73,6 @@ void loop() {
   }
 
   // Print axis values to Bluetooth and Serial
-  // SerialBT.print("X-Value: ");
-  // SerialBT.println(xValue);
-  // SerialBT.println("----------------------------------");
-  // SerialBT.print("Y-Value: ");
-  // SerialBT.println(yValue);
-  // SerialBT.println("----------------------------------");
-  // SerialBT.print("Z-Value: ");
-  // SerialBT.println(zValue);
-  // SerialBT.println("----------------------------------");
-
   Serial.print("X-axis: ");
   Serial.println(xValue);
   Serial.print("Y-axis: ");
@@ -82,33 +80,39 @@ void loop() {
   Serial.print("Z-axis: ");
   Serial.println(zValue);
   delay(200);
+
   // Detect jump based on z-axis acceleration
   if (zValue > jumpThreshold) {
     SerialBT.println("Jump detected! Yahoo! ^^");
     Serial.println("Jump detected! Yahoo! ^^");
+    myDFPlayer.play(2); // Play the fourth track on the DFPlayer Mini
     delay(2000);
   }
   
   if (yValue > jumpThreshold) {
     SerialBT.println("Lateral- Right Movement Detected");
     Serial.println("Lateral- Right Movement Detected");
+    myDFPlayer.play(2); // Play the second track on the DFPlayer Mini
     delay(2000);
   }
   if (yValue < fallThreshold) {
     SerialBT.println("Lateral-Left Movement Detected");
     Serial.println("Lateral Movement Detected");
+    myDFPlayer.play(2); // Play the third track on the DFPlayer Mini
     delay(2000);
   }
   if (xValue > jumpThreshold) {
     SerialBT.println("Fowardsies? detected! Yahoo! ^^");
     Serial.println("Fowardsies? Yahoo! ^^");
+    myDFPlayer.play(2); // Play the fifth track on the DFPlayer Mini
     delay(2000);
   }
   if (xValue < fallThreshold) {
     SerialBT.println("Backsies? detected! Yahoo! ^^");
     Serial.println("Backsies? Yahoo! ^^");
+    myDFPlayer.play(2); // Play the sixth track on the DFPlayer Mini
     delay(2000);
   }
-  delay(20);
-   // Adjust delay as needed
+
+  delay(20); // Adjust delay as needed
 }
